d102d52d0563fd9c5a01c7d6f20056909333e9b0
Emmyy882 <emmaagbaje3@gmail.com>
2023-09-24 13:52:39 +0100

    sorting algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array of integers using the Bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* swaaping elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				
+				/* printing the current order of ellements of the arrray */
+				print_array(array, size);
+			}
+		}
+	}
+
+}
