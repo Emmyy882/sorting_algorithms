c5529ab2be7b87ced151867d67b7cd33c6514ee5
Emmyy882 <emmaagbaje3@gmail.com>
2023-09-24 21:17:25 +0100

    sorting algorithms

--- sort.h
+++ sort.h
@@ -25,8 +25,12 @@ typedef struct listint_s
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void swap_items(int *array, size_t l, size_t r);
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size);
 
 void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *l, listint_t *r);
+void insertion_sort_list2(listint_t **list)
 
 void selection_sort(int *array, size_t size);
 
