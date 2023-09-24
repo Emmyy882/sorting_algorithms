868a7a8319f8d0bbcb570f01261e203cbc49db95
Emmyy882 <emmaagbaje3@gmail.com>
2023-09-24 21:48:09 +0100

    sorting algorithms

--- sort.h
+++ sort.h
@@ -35,4 +35,11 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size);
 
+void shell_sort(int *array, size_t size);
+
+void swap_nodes(listint_t **list, listint_t *l, listint_t *r);
+listint_t *get_tail(listint_t **list);
+void cocktail_sort_list(listint_t **list);
+
+void counting_sort(int *array, size_t size);
 #endif
